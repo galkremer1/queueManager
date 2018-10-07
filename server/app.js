b6954179fbdb1abeb1f8e08f4706dda78fb21490
galkremer1 <galkremer1@walla.co.il>
2018-10-08 00:11:32 +0300

    added bootstarp and removed unused components

--- server/app.js
+++ server/app.js
@@ -3,12 +3,19 @@ var kue = require('kue')
   ;
 let express = require('express');
 let app = express();
+var cors = require('cors');
 const MAXQUEUE = 10;
-const JOBTIME = 10;
+const JOBTIME = 1000 * 15;
+const NETWORKTIME = 1000;
+const AUTOJOBTIMEOUT = 10 * 1000;
+
+app.use(cors())
+app.options('/task', cors()); // enable pre-flight request for DELETE request
 
 
 function newJob (name, priority){
-  name = name || 'Default_Name';
+  console.log('Creating a new ' + name);
+  name = name || 'Default_Name'; 
   priority = priority || 'normal';
   var job = jobs.create('new job', {
     name: name
@@ -18,7 +25,7 @@ function newJob (name, priority){
       console.log('Job', job.id, 'with name', job.data.name, 'is done');
       setTimeout(()=>{
         newJob('Automatic Job');
-      }, 10000)
+      }, AUTOJOBTIMEOUT)
     })
     .on('failed', function (){
       console.log('Job', job.id, 'with name', job.data.name, 'has failed');
@@ -33,6 +40,7 @@ jobs.process('new job', MAXQUEUE ,function (job, done){
 });
 
 
+
 jobs.activeCount( function( err, total ) { // others are activeCount, completeCount, failedCount, delayedCount
     console.log('total is ', total);
     if( total == 0 ) {
@@ -41,9 +49,36 @@ jobs.activeCount( function( err, total ) { // others are activeCount, completeCo
     }
   });
 
+async function checkJobStatus(deactivate) {
+   await jobs.active( async function( err, ids) {
+    return await ids.forEach( async function( id ) {
+      await kue.Job.get( id, async function( err, job ) {
+        deactivate && job.inactive();
+      });
+    });
+  });
+}
+
 app.get('/task', function (req, res) {
   console.log('got a request');
   newJob('Manual Job', 'high');
-  res.send('test');
+  jobs.activeCount( function( err, total ) { // others are activeCount, completeCount, failedCount, delayedCount
+    if (total <= MAXQUEUE) {
+      setTimeout(()=>{
+        res.sendStatus(200);
+      }, NETWORKTIME)
+    } else {
+      //Over the maximum allowed queue
+      res.sendStatus(400);
+    }
+  });
 })
+
+app.get('/queue', function (req, res) {
+  checkJobStatus(false).then((data)=>{
+    console.log(data);
+    res.send(JSON.stringify({data}));
+  })
+})
+checkJobStatus(true);
 app.listen(5000);
--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/lib/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Header from './components/Header';
 
 //import  from './components'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            creatingTask: false, 
+            response: ''
+        };
+    }
+
+    creatManualTask = () => {
+        this.setState({creatingTask: true, response: ''});
+        axios.get('http://localhost:5000/task')
+        .then(response => {
+            this.setState({
+                response: response.status,
+                creatingTask: false
+            });
+        }).catch((err)=> {
+            this.setState({
+                response: err.response.status,
+                creatingTask: false
+            });
+        });
+    }
+    listCurrentJobs = () => {
+        axios.get('http://localhost:5000/queue')
+        .then(response => {
+            console.log('res: ', response);
+            this.setState({
+                currentJobs: response,
+            });
+        }).catch((err)=> {
+            console.err(err);
+        });
+    }
     render() {
-        const pathname = window.location.pathname
         return ( 
-            <div>
-            { !pathname.includes('editor') ? <Header /> : '' }
-                <div>hello</div>
+            <div className="main-container">
+                <Button bsStyle="primary" bsSize="large" block disabled={this.state.creatingTask} onClick={this.creatManualTask}>Creat A Manual Task</Button>
+                {
+                    this.state.response && 
+                    <div>
+                        Response: {this.state.response}
+                    </div>
+                }
+                <Button bsStyle="primary" bsSize="large" block  onClick={this.listCurrentJobs}>Get Current Job Queue</Button>
+
             </div>
         );
     }
